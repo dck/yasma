e34ecfaf2ababcbbfb16194df0b02abab93a0fae
Denis Usanov <usanovdd@gmail.com>
2013-04-26 00:32:59 +0400

    Added templates for other pages

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.configure('production', function(){
 
 // Routes
 app.get('/', routes.index);
+app.get('/graphs', routes.graphs);
+app.get('/stats', routes.stats);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', req.app.settings.config)
 };
+
+exports.graphs = function(req, res){
+  res.render('graphs', req.app.settings.config)
+};
+
+exports.stats = function(req, res){
+  res.render('stats', req.app.settings.config)
+};
