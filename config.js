27b350a751e0345c8e63a7d0e893f6238ee0a0ea
Denis Usanov <usanovdd@gmail.com>
2013-04-26 00:21:36 +0400

    Menu was added

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ var config = {};
 config.title = "YASMA";
 config.logo = "YASMA";
 config.description = "Yet Another Statistics for Mobile Apps";
+config.menu = ['users', 'graphics', 'stats'];
 
 module.exports = config;
