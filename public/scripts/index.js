00430069364564da180ae1df88429b8528295bb4
Denis Usanov <usanovdd@gmail.com>
2013-05-29 14:40:22 +0400

    Userlist is and userstats in in google table chart now

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,40 +6,24 @@ $( document ).ready(function() {
         if (this.selectedIndex) {
             ///playerstats/:game
             $.get('/playerstats/' + game, function(data) {
-                console.log(data);
-                // var idhead = document.createElement("div")
-                // idhead.className = "idhead";
-                // idhead.innerHTML = "Place";
-                // var namehead = document.createElement("div")
-                // namehead.className = "namehead";
-                // namehead.innerHTML = "Name";
-                // var scorehead = document.createElement("div")
-                // scorehead.className = "scorehead";
-                // scorehead.innerHTML = "Score";
-                // var clear = document.createElement("div")
-                // clear.id = "clear";
-                // $("#userlist").append(idhead).append(namehead).append(scorehead).append(clear);
 
-                drawUserTable();
+                drawUserTable(data);
+
             });
-
         }
     });
 });
 
 
-function drawUserTable(data) {
+function drawUserTable(users) {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Name');
-    data.addColumn('number', 'Salary');
-    data.addColumn('boolean', 'Full Time Employee');
-    data.addRows([
-        ['Mike',  {v: 10000, f: '$10,000'}, true],
-        ['Jim',   {v:8000,   f: '$8,000'},  false],
-        ['Alice', {v: 12500, f: '$12,500'}, true],
-        ['Bob',   {v: 7000,  f: '$7,000'},  true]
-    ]);
-
+    data.addColumn('number', 'Score');
+    var rows = [];
+    for (var i = 0; i<users.length; i++) {
+        rows.push([users[i].user, users[i].score]);
+    }
+    data.addRows(rows);
 
     var table = new google.visualization.Table(document.getElementById('userlist'));
     table.draw(data, {showRowNumber: true});
