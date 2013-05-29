ce13d3263c67cd170fadbe3b3acc384f7485ee78
Denis Usanov <usanovdd@gmail.com>
2013-05-29 13:24:57 +0400

    Includes google chart

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,24 @@
 $( document ).ready(function() {
     $('#apppicker').change(function() {
         var game = this.value;
-        ///playerstats/:game
-        $.get('/playerstats/' + game, function(data) {
-            console.log(data);
-        });
+        if (this.selectedIndex) {
+            ///playerstats/:game
+            $.get('/playerstats/' + game, function(data) {
+                console.log(data);
+                var idhead = document.createElement("div")
+                idhead.className = "idhead";
+                idhead.innerHTML = "Place";
+                var namehead = document.createElement("div")
+                namehead.className = "namehead";
+                namehead.innerHTML = "Name";
+                var scorehead = document.createElement("div")
+                scorehead.className = "scorehead";
+                scorehead.innerHTML = "Score";
+                var clear = document.createElement("div")
+                clear.id = "clear";
+                $("#userlist").append(idhead).append(namehead).append(scorehead).append(clear);
+                
+            });
+        }
     });
 });
